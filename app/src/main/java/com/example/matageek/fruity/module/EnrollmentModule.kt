9bee3a5d332ec5c093dac03c7409b56943d62936
nishinohi <tomoya0209@gmail.com>
2021-03-12 11:53:39 +0900

    add enroll response check

--- app/src/main/java/com/example/matageek/fruity/module/EnrollmentModule.kt
+++ app/src/main/java/com/example/matageek/fruity/module/EnrollmentModule.kt
@@ -1,10 +1,7 @@
 package com.example.matageek.fruity.module
 
 import android.util.Log
-import com.example.matageek.fruity.types.ConnPacketHeader
-import com.example.matageek.fruity.types.ConnectionMessageTypes
-import com.example.matageek.fruity.types.FmTypes
-import com.example.matageek.fruity.types.MessageType
+import com.example.matageek.fruity.types.*
 import no.nordicsemi.android.ble.data.Data
 import java.nio.ByteBuffer
 import java.nio.ByteOrder
@@ -93,7 +90,54 @@ class EnrollmentModule : Module("enroll", FmTypes.ModuleId.ENROLLMENT_MODULE.id)
         SET_ENROLLMENT_BROADCAST_APP_START_RESPONSE(7),
     }
 
+    enum class EnrollmentResponseCode(val code: Byte) {
+        OK(0x00),
+
+        // There are more enroll response codes that are taken from the Flash Storage response codes
+        ALREADY_ENROLLED_WITH_DIFFERENT_DATA(0x10),
+        PREENROLLMENT_FAILED(0x11),
+        SIG_CONFIGURATION_INVALID(0x12),
+        INCORRECT_NODE_ID(0x13),
+        HIGHEST_POSSIBLE_VALUE(0xFF.toByte())
+    }
+
+    class EnrollmentModuleEnrollmentResponse(packet: ByteArray) {
+        val serialNumberIndex: Int
+        val enrollmentResponseCode: Byte
+
+        companion object {
+            const val SIZE_OF_PACKET = 5
+        }
+
+        init {
+            if (packet.size < SIZE_OF_PACKET) throw Exception("invalid packet")
+            val byteBuffer = ByteBuffer.wrap(packet).order(ByteOrder.LITTLE_ENDIAN)
+            serialNumberIndex = byteBuffer.int
+            enrollmentResponseCode = byteBuffer.get()
+        }
+    }
+
     override fun actionResponseMessageReceivedHandler(packet: ByteArray) {
         Log.d("MATAG", "actionResponseMessageReceivedHandler: ${Data(packet)}")
+        if (packet.size < ConnPacketModule.SIZEOF_PACKET + EnrollmentModuleEnrollmentResponse.SIZE_OF_PACKET)
+            throw Exception("invalid packet")
+
+        val connModule = ConnPacketModule.readFromBytePacket(packet.copyOfRange(0,
+            ConnPacketModule.SIZEOF_PACKET)) ?: throw Exception("invalid packet")
+
+        val response = packet.copyOfRange(ConnPacketModule.SIZEOF_PACKET,
+            ConnPacketModule.SIZEOF_PACKET + EnrollmentModuleEnrollmentResponse.SIZE_OF_PACKET)
+        val enrollResponse = EnrollmentModuleEnrollmentResponse(response)
+
+        when (connModule.actionType) {
+            EnrollmentModuleActionResponseMessages.ENROLLMENT_RESPONSE.type -> {
+                when (enrollResponse.enrollmentResponseCode) {
+                    EnrollmentResponseCode.PREENROLLMENT_FAILED.code -> {
+                        Log.d("MATAG", "NodeId:${connModule.header.sender} enroll failed")
+                    }
+                }
+            }
+        }
+
     }
 }
